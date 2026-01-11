47d7391c6b9ad3c85162a04610014a0f573d8684
dnb <234731830+DoNotByte@users.noreply.github.com>
2026-01-11 14:00:57 +1100

    Established
    - Colors
    - Strings & titles
    - Created core activities and views
    - Created NavigationManager.java which includes pattern and functions to quickly change intents

--- app/src/main/java/com/example/ict372/ProductDetailsAct.java
+++ app/src/main/java/com/example/ict372/ProductDetailsAct.java
@@ -8,19 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ProductDetailsAct extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_product_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
 }
-
